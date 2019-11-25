594faff39473295bb991316adc1c9a6c6a6e5ce8
Mitchwatts93 <31891580+Mitchwatts93@users.noreply.github.com>
2019-11-25 13:22:54 +0000

    pypi setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 def readme():
     with open("README.rst") as f:
         return f.read()
 
 setup(
-    name='lyrics_processor',
-    packages = ['lyrics_processor'], 
-    python_requires='>3.7',
-    version='0.1',
+    name='lyricsprocessor',
+    packages = find_namespace_packages(), 
+    python_requires='>=3.7',
+    version='0.1.18',
     description='Lyrics processing code',
     long_description=readme(),
     classifiers=[
@@ -18,16 +18,12 @@ setup(
     ],
     keywords='lyricsgenius genius lyrics',
     url='https://github.com/Mitchwatts93/lyrics_generator',
-    download_url = 'https://github.com/Mitchwatts93/lyrics_generator/archive/v_01.tar.gz',
     author='https://github.com/Mitchwatts93',
     author_email='',
     license='MIT',
-    packages=find_packages(),
     install_requires = [
     'pandas>=0.23.4',
     'argparse>=1.1',
-    're>=2.2.1',
-    'json>=2.0.9',
     'lyricsgenius>=1.8.0'
         ],
     include_package_data=True,
